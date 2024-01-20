f81ea66a592f5aa8d754fa2a96e09e12bd91d64e
wxq <wxq@yeeaf.com>
2024-01-20 17:47:15 +0800

    添加依赖

---
+++ src/pages/index/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useUn } from '@uni-helper/uni-network/composables';
+const { data } = useUn();
+</script>
+
+<template>
+  <view>测试</view>>
+</template>
+
+<style  scoped></style>
